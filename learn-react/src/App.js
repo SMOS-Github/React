6de0016ae4e7c40f21b34aad302521f6ad2a423c
SM ORIGINALS <SMOS-Github@users.noreply.github.com>
2024-12-06 21:25:43 +0530

    Initial commit

---
+++ learn-react/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import xr from './module2.mjs';
+
+function App() {
+  return (
+      <>
+        <div className="Alex">Hello World</div>
+      </>
+  );
+}
+
+export default App;
