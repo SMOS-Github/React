f605f400181ff36344fae462f8b0ed74f558e2a7
SM ORIGINALS <SMOS-Github@users.noreply.github.com>
2024-12-14 01:48:16 +0530

    update
    Darkmode in progress

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextArea from './components/TextArea';
+import Darkmode from './components/Darkmode';
 
 function App() {
     return (
         <>  <Navbar/>
-            <TextArea/>
+            <TextArea />
+            <Darkmode/>
         </>
     );
 
--- learn-react/src/components/TextArea.js
+++ learn-react/src/components/TextArea.js
@@ -1,13 +1,12 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import Darkmode from './Darkmode';
 export default function TextArea(props) {
 
     const [def, set] = useState('');
 
     const words = def.trim().split(/\s+/).length;   //Regex syntex inside the line.
     const characters = def.length;
-
-    let darkMode = false;
 
     function OnClickChange(e) {
         set(e.target.value);
@@ -19,25 +18,14 @@ export default function TextArea(props) {
     function ChangeLower(e) {
         set(def.toLowerCase());
     }
-    function DarkMode() {
-        
-        darkMode = !darkMode;
-        document.body.style.backgroundColor = darkMode ? 'black' : 'white';
-        document.body.style.color = darkMode ? 'white' : 'black';
-
-        const textArea = document.getElementById('exampleFormControlTextarea1');
-        if (textArea) {
-            textArea.classList.toggle('bgh', darkMode);
-        }
-    }
     return (
         <>
+            <Darkmode/>
             <div className="container">
                 <h1 id="heading">{props.title}</h1>
                 <textarea className="form-control fs-4" id="exampleFormControlTextarea1" placeholder="Enter Text's to change Uppercase" rows="8" value={def} onChange={OnClickChange}> </textarea>
                 <button type="button" onClick={ChangeCase} className="btn btn-primary m-1">Upper Case</button>
                 <button type="button" onClick={ChangeLower} className="btn btn-primary m-1">Lower Case</button>
-                <button type="button" className="btn btn-dark m-1" onClick={DarkMode}>Dark Mode</button>
 
                 <div>
                     <p><b>No of Words : {words} <br /> and no of Charecters : {characters} <br /> Read Time : {0.008 * characters} </b>
